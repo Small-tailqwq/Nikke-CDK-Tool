d15084414ac6a0aefeb4b141c0089925c839876a
Small-tailqwq <56996438+Small-tailqwq@users.noreply.github.com>
2026-06-03 12:38:57 +0800

    chore: 迁移 ESLint v9 flat config 格式

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import { FlatCompat } from '@eslint/eslintrc'
+import js from '@eslint/js'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
+
+export default [
+  ...compat.config({
+    root: true,
+    env: { browser: true, es2021: true, node: true },
+    extends: [
+      'eslint:recommended',
+      'plugin:vue/vue3-recommended',
+      '@vue/eslint-config-prettier',
+    ],
+    parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    },
+  }),
+]
